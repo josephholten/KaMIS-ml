4b1531a43983f8d594d67ea61b753704739ab85e
Joseph Holten <joseph@holten.com>
2022-02-05 23:19:33 +0100

    chart_results.py reads logs now

--- misc/chart_results.py
+++ misc/chart_results.py
@@ -1,41 +1,68 @@
 import json
+import sys
+
 import numpy as np
 import os
 from pprint import pprint
 import matplotlib.pyplot as plt
 
 
+def parse(name):
+    if line.startswith(name + " "):
+        return line.removeprefix(name + " ")
+    else:
+        return None
+
+
 output_directory = "/home/jholten/perf_tests_kamis_ml"
-test_number = "27"
+test_number = "32"
 
 # evaluate tests
 with open("/home/jholten/KaMIS-ml/optima.txt") as optimum_file:
     split_lines = map(lambda line: line.split(" "), optimum_file.readlines())
     optimum = dict(map(lambda l: (l[0].removeprefix("/home/jholten/test_graphs/"), int(l[1])), split_lines))
 
-ratio = dict()
+info = dict()
 
 # has not run before
-if not os.path.isfile(f"{output_directory}/{test_number}/results.json"):
-    print("Computing MIS size...")
-    for output_filename in os.listdir(f"{output_directory}/{test_number}"):
-        if output_filename.endswith(".mis"):
-            output_filepath = os.path.join(output_directory, test_number, output_filename)
-            graph = output_filename.removesuffix(".mis")
-            ml = np.loadtxt(output_filepath)
-            ratio[graph] = np.sum(ml) / optimum[graph]
+if not os.path.isfile(f"{output_directory}/{test_number}/info.json"):
+    if os.path.isfile(f"{output_directory}/{test_number}/log.txt"):
+        with open(f"{output_directory}/{test_number}/log.txt") as log_file:
+            current_info = dict()
+            current_graph = ""
+            for line in log_file:
+                if value := parse("graph"):
+                    current_graph = value
+                    info[current_graph] = {}
+                    current_info = info[current_graph]
 
-    with open(f"{output_directory}/{test_number}/results.json", "w") as out_file:
-        json.dump(ratio, out_file)
+                if value := parse("MIS_weight"):
+                    current_info["MIS_weight"] = int(value)
+                    current_info["ratio"] = int(value) / optimum[current_graph]
+
+                if value := parse("reduction_time"):
+                    current_info["time"] = float(value)
+
+                if value := parse("ls_std_dev"):
+                    try:
+                        current_info["ls_std_dev"].append(float(value))
+                    except KeyError as error:
+                        current_info["ls_std_dev"] = [float(value)]
+        with open(f"{output_directory}/{test_number}/info.json", "w") as info_file:
+            json.dump(info, info_file)
+    else:
+        print("no log to parse, exiting ...")
+        sys.exit(1)
 
 # has run before, use saved results
 else:
     print("Reading saved results")
-    with open(f"{output_directory}/{test_number}/results.json") as file:
-        ratio = json.load(file)
+    with open(f"{output_directory}/{test_number}/info.json") as file:
+        info = json.load(file)
 
-pprint(ratio)
-names, values = zip(*sorted(ratio.items(), key=lambda tup: tup[0]))
+pprint(info)
+names, values = zip(*sorted(info.items(), key=lambda tup: tup[0]))
 names = list(map(lambda s: s.removesuffix("-sorted.graph"), names))
-plt.bar(names, values)
+plt.title("Ratios to optimum")
+plt.bar(names, map(lambda x: x["ratio"]))
 plt.show()
