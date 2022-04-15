2c620162d5315e87de395683958c69434a978271
Joseph Holten <joseph@holten.com>
2022-04-15 17:26:55 +0200

    cleaned up the macros

--- wmis/lib/mis/ml/feature_set_macros.h
+++ wmis/lib/mis/ml/feature_set_macros.h
@@ -16,74 +16,72 @@
  */
 
 #ifndef FEATURE_SET
-#define FEATURE_SET LARGE
+    #define FEATURE_SET LARGE
 #endif
 
 #if FEATURE_SET == LARGE
-#define F_NODES
-#define F_EDGES
-#define F_DEG
-#define F_CHI2_DEG
-#define F_AVG_CHI2_DEG
-#define F_LCC
-#define F_CHI2_LCC
-#define F_CHROMATIC
-#define F_T_WEIGHT
-#define F_NODE_W
-#define F_W_DEG
-#define F_CHI2_W_DEG
-#define F_MAX_W
-#define F_MIN_W
-#define F_MAX_NEIGHBORHOOD_W
-#define F_HT
-#endif
-
-#if FEATURE_SET == GREEDY
-#define F_NODES
-#define F_EDGES
-#define F_DEG
-#define F_T_WEIGHT
-#define F_NODE_W
-#define F_W_DEG
-#define F_HT
-#endif
-
-#if FEATURE_SET == WITH_LS
-#define F_NODES
-#define F_EDGES
-#define F_DEG
-#define F_CHI2_DEG
-#define F_AVG_CHI2_DEG
-#define F_LCC
-#define F_CHI2_LCC
-#define F_CHROMATIC
-#define F_T_WEIGHT
-#define F_NODE_W
-#define F_W_DEG
-#define F_CHI2_W_DEG
-#define F_MAX_W
-#define F_MIN_W
-#define F_MAX_NEIGHBORHOOD_W
-#define F_HT
-#define F_LS
+    #define F_NODES
+    #define F_EDGES
+    #define F_DEG
+    #define F_CHI2_DEG
+    #define F_AVG_CHI2_DEG
+    #define F_LCC
+    #define F_CHI2_LCC
+    #define F_CHROMATIC
+    #define F_T_WEIGHT
+    #define F_NODE_W
+    #define F_W_DEG
+    #define F_CHI2_W_DEG
+    #define F_MAX_W
+    #define F_MIN_W
+    #define F_MAX_NEIGHBORHOOD_W
+    #define F_HT
+#elif FEATURE_SET == GREEDY
+    #define F_NODES
+    #define F_EDGES
+    #define F_DEG
+    #define F_T_WEIGHT
+    #define F_NODE_W
+    #define F_W_DEG
+    #define F_HT
+#elif FEATURE_SET == WITH_LS
+    #define F_NODES
+    #define F_EDGES
+    #define F_DEG
+    #define F_CHI2_DEG
+    #define F_AVG_CHI2_DEG
+    #define F_LCC
+    #define F_CHI2_LCC
+    #define F_CHROMATIC
+    #define F_T_WEIGHT
+    #define F_NODE_W
+    #define F_W_DEG
+    #define F_CHI2_W_DEG
+    #define F_MAX_W
+    #define F_MIN_W
+    #define F_MAX_NEIGHBORHOOD_W
+    #define F_HT
+    #define F_LS
+#else
+    static_assert(false, "Wrong feature set selected!");
 #endif
 
 
 // dependencies
 #ifdef F_CHI2_W_DEG
-#define F_W_DEG
+    #define F_W_DEG
 #endif
 #ifdef F_HT
-#define F_W_DEG
+    #define F_W_DEG
 #endif
 #ifdef F_AVG_CHI2_DEG
-#define F_CHI2_DEG
+    #define F_CHI2_DEG
 #endif
 #ifdef F_CHI2_DEG
-#define F_DEG
+    #define F_DEG
 #endif
 #ifdef F_CHI2_LCC
-#define F_LCC
+    #define F_LCC
 #endif
 
 #endif //KAMIS_FEATURE_SET_MACROS_H
