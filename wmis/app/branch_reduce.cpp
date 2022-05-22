0966f5b3103615b2833fc730905e87d740b1844e
Joseph Holten <joseph@holten.com>
2022-05-22 11:43:33 +0200

    fixed filepath validation and added logging of solution filepath

--- wmis/app/branch_reduce.cpp
+++ wmis/app/branch_reduce.cpp
@@ -128,10 +128,7 @@ int main(int argn, char **argv) {
                 // std::cout << "MIS_weight_check " << is_weight << std::endl;
         }
 
-        if (mis_config.write_graph) {
-            graph_io::writeIndependentSet(G, mis_config.output_filename);
-            algo_log::logger()["solution_file"] = mis_config.output_filename;
-        }
+        if (mis_config.write_graph) graph_io::writeIndependentSet(G, mis_config.output_filename);
 
         std::cout << std::setw(2) << algo_log::logger() << std::endl;
         return 0;
